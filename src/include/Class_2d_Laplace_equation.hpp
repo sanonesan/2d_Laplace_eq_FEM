004e78f589ca88a62eedb2ecb050ca7ebf579481
sanonesan <ezcudze@gmail.com>
2023-05-27 23:30:07 +0300

    stiffness matrix

--- src/include/Class_2d_Laplace_equation.hpp
+++ src/include/Class_2d_Laplace_equation.hpp
@@ -28,15 +28,25 @@ class Class_2d_Laplace_equation{
         // std::vector<std::vector<T>> _dirichlet_upper_boundary_nodes;
         // std::vector<std::vector<T>> _dirichlet_lower_boundary_nodes;
         std::vector<std::vector<T>> _boundary_nodes;
-        std::vector<std::vector<T>> _dirichlet_left_boundary_edges;
-        std::vector<std::vector<T>> _dirichlet_right_boundary_edges;
-        std::vector<std::vector<T>> _dirichlet_upper_boundary_edges;
-        std::vector<std::vector<T>> _dirichlet_lower_boundary_edges;
+        std::vector<std::vector<std::size_t>> _dirichlet_left_boundary_edges;
+        std::vector<std::vector<std::size_t>> _dirichlet_right_boundary_edges;
+        std::vector<std::vector<std::size_t>> _dirichlet_upper_boundary_edges;
+        std::vector<std::vector<std::size_t>> _dirichlet_lower_boundary_edges;
 
+        std::function<T (const T x, const T y)> _dirichlet_lower_boundary_condition;
+        std::function<T (const T x, const T y)> _dirichlet_upper_boundary_condition;
 
         Class_2d_Laplace_equation<T> DEFAULT_TEST(){
 
+            this->_dirichlet_lower_boundary_condition = [](const T x, const T y){
+                return 10.;
+            };
 
+            this->_dirichlet_upper_boundary_condition = [](const T x, const T y){
+                return 0.;
+            };
+
+            
 
             std::ifstream fin;
 
@@ -80,7 +90,6 @@ class Class_2d_Laplace_equation{
                 //std::cout << p1_p2_p3[0] << "\t" << p1_p2_p3[1] << "\t" << p1_p2_p3[2] << "\n";
             }            
             fin.close();
-
             //check for 1 triangle element
             // std::cout << this->_polygons[0][0] << "\t" << this->_polygons[0][1] << "\t" << this->_polygons[0][2] << "\n";
             // std::cout << this->_nodes[this->_polygons[0][0]][0] << "\t" << this->_nodes[this->_polygons[0][0]][1] << "\n";
@@ -102,30 +111,94 @@ class Class_2d_Laplace_equation{
             fin.close();
 
 
-
-            //Reading edges of boundary conditions 
-
-            // fin.open("meshes/mesh/boundaries/edges/mesh_left_boundary_edges.txt");
-            // if (!fin.is_open()) {
-            //     throw std::invalid_argument("Smth's wrong with path");
-            // }            
-
-            // std::vector<std::size_t> p1_p2 = {0, 0};
-            // p1_p2_p3.shrink_to_fit();
+            //Reading boundaty edges
+            //2D array contains positions of elements in vector<T> _nodes (mesh_nodes.txt)
+            //which form a triangulars
+            std::vector<std::size_t> p1_p2 = {0, 0};
+            p1_p2.shrink_to_fit();
             
-            // while (fin >> p1_p2_p3[0] >> p1_p2_p3[1] >> p1_p2_p3[2]) {
+            //left boundary
+
+            fin.open("/home/san/Code/Laplace_eq/meshes/mesh/boundaries/edges/mesh_left_boundary_edges.txt");
+            if (!fin.is_open()) {
+                throw std::invalid_argument("Smth's wrong with path");
+            }            
+            
+            while (fin >> p1_p2[0] >> p1_p2[1]) {
                 
-            //     //minus 1 as Wolfram Mathematica numerates nodes from 1 to n
-            //     //we need from 0 to n-1
+                //minus 1 as Wolfram Mathematica numerates nodes from 1 to n
+                //we need from 0 to n-1
 
-            //     p1_p2_p3[0] -= 1;
-            //     p1_p2_p3[1] -= 1;
-            //     p1_p2_p3[2] -= 1;
+                p1_p2[0] -= 1;
+                p1_p2[1] -= 1;
 
-            //     this->_polygons.push_back(p1_p2_p3);
-            //     //std::cout << p1_p2_p3[0] << "\t" << p1_p2_p3[1] << "\t" << p1_p2_p3[2] << "\n";
-            // }            
-            // fin.close();
+                this->_dirichlet_left_boundary_edges.push_back(p1_p2);
+                //std::cout << p1_p2_p3[0] << "\t" << p1_p2_p3[1] << "\t" << p1_p2_p3[2] << "\n";
+            }            
+            fin.close();
+
+
+            //right boundary
+
+            fin.open("/home/san/Code/Laplace_eq/meshes/mesh/boundaries/edges/mesh_right_boundary_edges.txt");
+            if (!fin.is_open()) {
+                throw std::invalid_argument("Smth's wrong with path");
+            }            
+            
+            while (fin >> p1_p2[0] >> p1_p2[1]) {
+                
+                //minus 1 as Wolfram Mathematica numerates nodes from 1 to n
+                //we need from 0 to n-1
+
+                p1_p2[0] -= 1;
+                p1_p2[1] -= 1;
+
+                this->_dirichlet_left_boundary_edges.push_back(p1_p2);
+                //std::cout << p1_p2_p3[0] << "\t" << p1_p2_p3[1] << "\t" << p1_p2_p3[2] << "\n";
+            }            
+            fin.close();
+
+            
+            //lower
+
+            fin.open("/home/san/Code/Laplace_eq/meshes/mesh/boundaries/edges/mesh_lower_boundary_edges.txt");
+            if (!fin.is_open()) {
+                throw std::invalid_argument("Smth's wrong with path");
+            }            
+            
+            while (fin >> p1_p2[0] >> p1_p2[1]) {
+                
+                //minus 1 as Wolfram Mathematica numerates nodes from 1 to n
+                //we need from 0 to n-1
+
+                p1_p2[0] -= 1;
+                p1_p2[1] -= 1;
+
+                this->_dirichlet_lower_boundary_edges.push_back(p1_p2);
+                //std::cout << p1_p2_p3[0] << "\t" << p1_p2_p3[1] << "\t" << p1_p2_p3[2] << "\n";
+            }            
+            fin.close();
+
+
+            //upper
+
+            fin.open("/home/san/Code/Laplace_eq/meshes/mesh/boundaries/edges/mesh_upper_boundary_edges.txt");
+            if (!fin.is_open()) {
+                throw std::invalid_argument("Smth's wrong with path");
+            }            
+            
+            while (fin >> p1_p2[0] >> p1_p2[1]) {
+                
+                //minus 1 as Wolfram Mathematica numerates nodes from 1 to n
+                //we need from 0 to n-1
+
+                p1_p2[0] -= 1;
+                p1_p2[1] -= 1;
+
+                this->_dirichlet_upper_boundary_edges.push_back(p1_p2);
+                //std::cout << p1_p2_p3[0] << "\t" << p1_p2_p3[1] << "\t" << p1_p2_p3[2] << "\n";
+            }            
+            fin.close();
 
 
             return *this; 
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,111 @@
 #include <vector>
 #include "./include/Solver_2d_Laplace_eq.hpp"
 
+#include "../../computational_methods_labs/structures/linalg/Matrix_n_Vector.hpp"
+
+template<typename T>
+class Element{
+    public:
+
+    T x1 = 0;
+    T x2 = 0;
+    T x3 = 0;
+
+    T y1 = 0;
+    T y2 = 0;
+    T y3 = 0;
+
+    std::vector<T> x;
+    std::vector<T> y;
+    std::vector<std::size_t> poly;
+    
+    
+};
+
+
+template<typename T> 
+void stiffness_matrix(Class_2d_Laplace_equation<T> laplace_eq){
+
+
+    auto ind_shift = [](std::size_t index){
+        if(index == 3){
+            return (std::size_t)0;
+        }
+        if(index == 4){
+            return (std::size_t)1;
+        }
+        
+        return index;
+    };
+
+
+    std::ofstream fout;
+    fout.open("./stiffness_matrix_elements_of_each_triangle.txt");
+    fout << std::scientific;
+    fout << std::setprecision(8);
+
+
+    Matrix<T> full_matrix(laplace_eq._nodes.size(), laplace_eq._nodes.size());
+    Vector<T> full_b(laplace_eq._nodes.size() - laplace_eq._boundary_nodes.size());
+
+    //check for first element
+    Matrix<T> A(3, 3);    
+    Element<T> el;
+    T S_el = 0.;
+
+    for(std::size_t k = 0; k < laplace_eq._polygons.size(); ++k){
+
+
+        for(std::size_t i = 0; i < 3; ++i){
+            el.x.push_back( laplace_eq._nodes[laplace_eq._polygons[k][i]][0] );
+            el.y.push_back( laplace_eq._nodes[laplace_eq._polygons[k][i]][1] );
+        }
+        el.poly = laplace_eq._polygons[k];
+
+        //std::cout << el.poly[0] << "\t" << el.poly[1] << "\t" << el.poly[2] << "\n";
+        // el.print();
+        S_el = ((el.x[1] - el.x[0]) * (el.y[2] - el.y[0]) - (el.x[2] - el.x[0]) * (el.y[1] - el.y[0])) / 2;
+        // std:: cout << S_el << "\n";
+        S_el = 1. / 4. / S_el;
+        // std:: cout << S_el << "\n";
+
+        
+
+        for(std::size_t i = 0; i < 3; ++i){
+            for(std::size_t j = 0; j < 3; ++j){            
+                A[i][j] = S_el * ( (el.y[ind_shift(i+1)] - el.y[ind_shift(i+2)]) * (el.y[ind_shift(j+1)] - el.y[ind_shift(j+2)]) + (el.x[ind_shift(i+2)] - el.x[ind_shift(i+1)]) * (el.x[ind_shift(j+2)] - el.x[ind_shift(j+1)]));
+                fout << A[i][j] << "\t";
+                full_matrix[laplace_eq._polygons[k][i]][laplace_eq._polygons[k][j]] += A[i][j];
+            }
+            fout << "\n";
+        }
+        fout << "\n";
+
+    }
+    fout.close();
+
+    fout.open("./full_matrix.txt");
+
+    for(std::size_t i = 0; i < full_matrix.get_rows(); ++i){
+        for(std::size_t j = 0; j < full_matrix.get_cols(); ++j){            
+            fout << full_matrix[i][j] << "\t";
+        }
+        fout << "\n";
+    }
+    fout << "\n";
+    
+    //std::cout << laplace_eq._polygons.size();
+    std::cout << laplace_eq._nodes.size() - laplace_eq._dirichlet_lower_boundary_edges.size() + laplace_eq._dirichlet_upper_boundary_edges.size();
+    //A.print();
+}
+
 int main(int args, char **argv){
 
     typedef double T;
     Class_2d_Laplace_equation<T> laplace_eq;
     laplace_eq.DEFAULT_TEST();
 
-
+    stiffness_matrix(laplace_eq);
     
     return 0;
 }
