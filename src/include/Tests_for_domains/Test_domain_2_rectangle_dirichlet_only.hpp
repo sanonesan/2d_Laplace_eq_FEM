67f9a9dd5c78bcd54bdff4920961c6b69af84018
sanonesan <ezcudze@gmail.com>
2024-01-22 13:03:09 +0300

    save

--- src/include/Tests_for_domains/Test_domain_2_rectangle_dirichlet_only.hpp
+++ src/include/Tests_for_domains/Test_domain_2_rectangle_dirichlet_only.hpp
@@ -1,205 +1,208 @@
 #pragma once
 
-#include<vector>
-#include<cmath>
-#include<functional> 
+#include <cmath>
+#include <functional>
+#include <vector>
 
 #include "../Class_2d_Laplace_equation.hpp"
 
-
 /**
- * Test in domain: 
+ * Test in domain:
  * {(x, y) | 0 <= y <= 2, x \in [0, 4]}
- * 
+ *
  * V(x, 2) = 0.
  * V(x, 0) = 10.
  * V(0., y) = V(4., y)
- * 
-*/
-template<class T>
-class Test_domain_2_rectangle_dirichlet_only: virtual public Class_2d_Laplace_equation<T>{
+ *
+ */
+template <class T>
+class Test_domain_2_rectangle_dirichlet_only
+    : virtual public Class_2d_Laplace_equation<T> {
 
+public:
+  Test_domain_2_rectangle_dirichlet_only() {
 
-
-    public:
-
-
-        Test_domain_2_rectangle_dirichlet_only(){  
-
-
-            auto exact_solution = [](T x, T y, std::size_t row_num_el = 350) -> T {
-                T res = 0.;
-                for (std::size_t n = 1; n < row_num_el + 1; ++n) {
-                    if ((n % 2) != 0 ) {
-                        res += 20 * (1 - cos(M_PI * n)) / M_PI / n / (exp(- M_PI * n / 2) - exp( M_PI * n / 2)) 
-                                * (exp(- M_PI * n / 4 * y) - exp( M_PI * n / 4 * y)) * sin(M_PI * n / 4 * x);
-                        }
-                    }                        
-                if (fabs(res) < 1e-15) return 0;
-                                
-                return res;
-                
-            };
-
-            
-            //test name
-            this->_name = std::string (__func__);
-
-            this->_dirichlet_lower_boundary_condition = [exact_solution](const T x, const T y){
-                return exact_solution(x, y);
-            };
-
-            this->_dirichlet_upper_boundary_condition = [exact_solution](const T x, const T y){
-                return exact_solution(x, y);
-            };
-
-            this->_dirichlet_left_boundary_condition = [exact_solution](const T x, const T y){
-                return exact_solution(x, y);
-            };
-
-            this->_dirichlet_right_boundary_condition = [exact_solution](const T x, const T y){
-                return exact_solution(x, y);
-            };
-
-
-
-            // Other boundary conditions
-
-            // this->_dirichlet_lower_boundary_condition = [exact_solution](const T x, const T y){
-            //     return 0.;
-            // };
-
-            // this->_dirichlet_upper_boundary_condition = [exact_solution](const T x, const T y){
-            //     return 10.;
-            // };
-
-            // this->_dirichlet_left_boundary_condition = [exact_solution](const T x, const T y){
-            //     return 0.;
-            // };
-
-            // this->_dirichlet_right_boundary_condition = [exact_solution](const T x, const T y){
-            //     return 0.;
-            // };
-
-            this->Set_domain(*this, "../domains/domain_2/mesh001");
+    auto exact_solution = [](T x, T y, std::size_t row_num_el = 350) -> T {
+      T res = 0.;
+      for (std::size_t n = 1; n < row_num_el + 1; ++n) {
+        if ((n % 2) != 0) {
+          res += 20 * (1 - cos(M_PI * n)) / M_PI / n /
+                 (exp(-M_PI * n / 2) - exp(M_PI * n / 2)) *
+                 (exp(-M_PI * n / 4 * y) - exp(M_PI * n / 4 * y)) *
+                 sin(M_PI * n / 4 * x);
         }
+      }
+      if (fabs(res) < 1e-15)
+        return 0;
 
-        
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_005(){
+      return res;
+    };
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_005");
-            this->Set_domain(*this, "../domains/domain_2/mesh005");
-            return *this;
+    // test name
+    this->_name = std::string(__func__);
+
+    this->_dirichlet_lower_boundary_condition =
+        [exact_solution](const T x, const T y) { return exact_solution(x, y); };
+
+    this->_dirichlet_upper_boundary_condition =
+        [exact_solution](const T x, const T y) { return exact_solution(x, y); };
+
+    this->_dirichlet_left_boundary_condition =
+        [exact_solution](const T x, const T y) { return exact_solution(x, y); };
+
+    this->_dirichlet_right_boundary_condition =
+        [exact_solution](const T x, const T y) { return exact_solution(x, y); };
+
+    // Other boundary conditions
+
+    // this->_dirichlet_lower_boundary_condition = [exact_solution](const T x,
+    // const T y){
+    //     return 0.;
+    // };
+
+    // this->_dirichlet_upper_boundary_condition = [exact_solution](const T x,
+    // const T y){
+    //     return 10.;
+    // };
+
+    // this->_dirichlet_left_boundary_condition = [exact_solution](const T x,
+    // const T y){
+    //     return 0.;
+    // };
+
+    // this->_dirichlet_right_boundary_condition = [exact_solution](const T x,
+    // const T y){
+    //     return 0.;
+    // };
+
+    this->Set_domain(*this, "../domains/domain_2/mesh001");
+  }
+
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_005() {
+
+    this->_name = std::string("Test_domain_2_rectangle_dirichlet_only_005");
+    this->Set_domain(*this, "../domains/domain_2/mesh005");
+    return *this;
+  }
+
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_005_rev_calfem() {
+
+    this->_name =
+        std::string("Test_domain_2_rectangle_dirichlet_only_005_rev_calfem");
+
+    auto exact_solution_rev = [](T x, T y, std::size_t row_num_el = 350) -> T {
+      T res = 0.;
+      for (std::size_t n = 1; n < row_num_el + 1; ++n) {
+        if ((n % 2) != 0) {
+          res += 20 * (1 - cos(M_PI * n)) / M_PI / n /
+                 (exp(-M_PI * n / 2) - exp(M_PI * n / 2)) *
+                 (exp(-M_PI * n / 4 * (2 - y)) - exp(M_PI * n / 4 * (2 - y))) *
+                 sin(M_PI * n / 4 * x);
         }
+      }
+      if (fabs(res) < 1e-15)
+        return 0;
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_005_rev_calfem(){
+      return res;
+    };
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_005_rev_calfem");
+    this->_dirichlet_lower_boundary_condition =
+        [exact_solution_rev](const T x, const T y) {
+          return exact_solution_rev(x, y);
+        };
 
-            auto exact_solution_rev = [](T x, T y, std::size_t row_num_el = 350) -> T {
-                T res = 0.;
-                for (std::size_t n = 1; n < row_num_el + 1; ++n) {
-                    if ((n % 2) != 0 ) {
-                        res += 20 * (1 - cos(M_PI * n)) / M_PI / n / (exp(- M_PI * n / 2) - exp( M_PI * n / 2)) 
-                                * (exp(- M_PI * n / 4 * (2 - y)) - exp( M_PI * n / 4 * (2 - y))) * sin(M_PI * n / 4 * x);
-                        }
-                    }                        
-                if (fabs(res) < 1e-15) return 0;
-                                
-                return res;
-                
-            };
+    this->_dirichlet_upper_boundary_condition =
+        [exact_solution_rev](const T x, const T y) {
+          return exact_solution_rev(x, y);
+        };
 
-            this->_dirichlet_lower_boundary_condition = [exact_solution_rev](const T x, const T y){
-                return exact_solution_rev(x, y);
-            };
+    this->_dirichlet_left_boundary_condition = [exact_solution_rev](const T x,
+                                                                    const T y) {
+      return exact_solution_rev(x, y);
+    };
 
-            this->_dirichlet_upper_boundary_condition = [exact_solution_rev](const T x, const T y){
-                return exact_solution_rev(x, y);
-            };
+    this->_dirichlet_right_boundary_condition =
+        [exact_solution_rev](const T x, const T y) {
+          return exact_solution_rev(x, y);
+        };
 
-            this->_dirichlet_left_boundary_condition = [exact_solution_rev](const T x, const T y){
-                return exact_solution_rev(x, y);
-            };
+    this->Set_domain(*this, "../domains/domain_2/mesh005_calfem");
+    return *this;
+  }
 
-            this->_dirichlet_right_boundary_condition = [exact_solution_rev](const T x, const T y){
-                return exact_solution_rev(x, y);
-            };
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_005_calfem() {
 
+    this->_name =
+        std::string("Test_domain_2_rectangle_dirichlet_only_005_calfem");
+    this->Set_domain(*this, "../domains/domain_2/mesh005_calfem");
+    return *this;
+  }
 
-            this->Set_domain(*this, "../domains/domain_2/mesh005_calfem");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_001() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_005_calfem(){
+    this->_name = std::string("Test_domain_2_rectangle_dirichlet_only_001");
+    this->Set_domain(*this, "../domains/domain_2/mesh001");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_005_calfem");
-            this->Set_domain(*this, "../domains/domain_2/mesh005_calfem");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_001_calfem() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_001(){
+    this->_name =
+        std::string("Test_domain_2_rectangle_dirichlet_only_001_calfem");
+    this->Set_domain(*this, "../domains/domain_2/mesh001_calfem");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_001");
-            this->Set_domain(*this, "../domains/domain_2/mesh001");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_0005_calfem() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_001_calfem(){
+    this->_name =
+        std::string("Test_domain_2_rectangle_dirichlet_only_0005_calfem");
+    this->Set_domain(*this, "../domains/domain_2/mesh0005_calfem");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_001_calfem");
-            this->Set_domain(*this, "../domains/domain_2/mesh001_calfem");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_0001() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_0005_calfem(){
+    this->_name = std::string("Test_domain_2_rectangle_dirichlet_only_0001");
+    this->Set_domain(*this, "../domains/domain_2/mesh0001");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_0005_calfem");
-            this->Set_domain(*this, "../domains/domain_2/mesh0005_calfem");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_0001_calfem() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_0001(){
+    this->_name =
+        std::string("Test_domain_2_rectangle_dirichlet_only_0001_calfem");
+    this->Set_domain(*this, "../domains/domain_2/mesh0001_calfem");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_0001");
-            this->Set_domain(*this, "../domains/domain_2/mesh0001");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00005_calfem() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_0001_calfem(){
+    this->_name =
+        std::string("Test_domain_2_rectangle_dirichlet_only_00005_calfem");
+    this->Set_domain(*this, "../domains/domain_2/mesh00005_calfem");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_0001_calfem");
-            this->Set_domain(*this, "../domains/domain_2/mesh0001_calfem");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00008() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00005_calfem(){
+    this->_name = std::string("Test_domain_2_rectangle_dirichlet_only_00008");
+    this->Set_domain(*this, "../domains/domain_2/mesh00008");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_00005_calfem");
-            this->Set_domain(*this, "../domains/domain_2/mesh00005_calfem");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00008_calfem() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00008(){
+    this->_name =
+        std::string("Test_domain_2_rectangle_dirichlet_only_00008_calfem");
+    this->Set_domain(*this, "../domains/domain_2/mesh00008_calfem");
+    return *this;
+  }
 
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_00008");
-            this->Set_domain(*this, "../domains/domain_2/mesh00008");
-            return *this;
-        }
+  Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00008_s() {
 
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00008_calfem(){
-
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_00008_calfem");
-            this->Set_domain(*this, "../domains/domain_2/mesh00008_calfem");
-            return *this;
-        }
-
-        Test_domain_2_rectangle_dirichlet_only<T> Set_mesh_00008_s(){
-
-            this->_name = std::string ("Test_domain_2_rectangle_dirichlet_only_00008_s");
-            this->Set_domain(*this, "../domains/domain_2/mesh00008_s");
-            return *this;
-        }
-
-
+    this->_name = std::string("Test_domain_2_rectangle_dirichlet_only_00008_s");
+    this->Set_domain(*this, "../domains/domain_2/mesh00008_s");
+    return *this;
+  }
 };
--- src/main.cpp
+++ src/main.cpp
@@ -1,221 +1,203 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
 #include <time.h>
+#include <vector>
 
-//Import Solver
+// Import Solver
 #include "./include/Solver_2d_Laplace_eq.hpp"
 
-//Import tests
-#include "./include/Tests_for_domains/Test_domain_1_sin.hpp"
-#include "./include/Tests_for_domains/Test_domain_1_1_sin.hpp"
+// Import tests
 #include "./include/Tests_for_domains/Test_domain_1_1_1_sin.hpp"
+#include "./include/Tests_for_domains/Test_domain_1_1_sin.hpp"
+#include "./include/Tests_for_domains/Test_domain_1_sin.hpp"
 #include "./include/Tests_for_domains/Test_domain_2_rectangle.hpp"
 #include "./include/Tests_for_domains/Test_domain_2_rectangle_dirichlet_only.hpp"
 #include "./include/Tests_for_domains/Test_domain_3_sin_sin.hpp"
 #include "./include/Tests_for_domains/Test_domain_4_abs_x.hpp"
 
+int main(int args, char **argv) {
 
-int main(int args, char **argv){
+  clock_t t_start, t_end;
 
-    clock_t t_start, t_end;
+  typedef double T;
 
+  Solver_2d_Laplace_eq<T> solver;
+  solver.tol = 1e-16;
+  solver.notifications = true;
 
-    typedef double T;
-    
-    Solver_2d_Laplace_eq<T> solver;
-    solver.tol = 1e-16;
-    solver.notifications = true;
+  t_start = clock();
 
+  // Test_domain_1_sin<T> test1;
 
-    t_start  = clock();
+  // test1.Set_mesh_01();
+  // solver.output_folder = "../output/domain_1/mesh01/";
+  // solver.file_name = test1._name;
+  // solver.solve_eq(test1);
 
-    // Test_domain_1_sin<T> test1;
+  // test1.Set_mesh_005();
+  // solver.output_folder = "../output/domain_1/mesh005/";
+  // solver.file_name = test1._name;
+  // solver.solve_eq(test1);
 
-    // test1.Set_mesh_01();
-    // solver.output_folder = "../output/domain_1/mesh01/";
-    // solver.file_name = test1._name;
-    // solver.solve_eq(test1);
+  // test1.Set_mesh_001();
+  // solver.output_folder = "../output/domain_1/mesh001/";
+  // solver.file_name = test1._name;
+  // solver.solve_eq(test1);
 
-    // test1.Set_mesh_005();
-    // solver.output_folder = "../output/domain_1/mesh005/";
-    // solver.file_name = test1._name;
-    // solver.solve_eq(test1);
+  // test1.Set_mesh_0001();
+  // solver.output_folder = "../output/domain_1/mesh0001/";
+  // solver.file_name = test1._name;
+  // solver.solve_eq(test1);
 
-    // test1.Set_mesh_001();
-    // solver.output_folder = "../output/domain_1/mesh001/";
-    // solver.file_name = test1._name;
-    // solver.solve_eq(test1);       
+  /*
+      Rectangle periodic
+  */
 
-    // test1.Set_mesh_0001();
-    // solver.output_folder = "../output/domain_1/mesh0001/";
-    // solver.file_name = test1._name;
-    // solver.solve_eq(test1);
+  // Test_domain_2_rectangle<T> test2;
+  // test2.Set_mesh_005();
+  // solver.output_folder = "../output/domain_2/mesh005/";
+  // solver.file_name = test2._name;
+  // solver.solve_eq(test2);
 
+  // test2.Set_mesh_005_calfem();
+  // solver.output_folder = "../output/domain_2/mesh005_calfem/";
+  // solver.file_name = test2._name;
+  // solver.solve_eq(test2);
 
-    /*
-        Rectangle periodic
-    */
+  // test2.Set_mesh_001();
+  // solver.output_folder = "../output/domain_2/mesh001_calfem/";
+  // solver.file_name = test2._name;
+  // solver.solve_eq(test2);
 
-    // Test_domain_2_rectangle<T> test2;
-    // test2.Set_mesh_005();    
-    // solver.output_folder = "../output/domain_2/mesh005/";
-    // solver.file_name = test2._name;
-    // solver.solve_eq(test2);
+  // test2.Set_mesh_0001();
+  // solver.output_folder = "../output/domain_2/mesh0001/";
+  // solver.file_name = test2._name;
+  // solver.solve_eq(test2);
 
-    // test2.Set_mesh_005_calfem();    
-    // solver.output_folder = "../output/domain_2/mesh005_calfem/";
-    // solver.file_name = test2._name;
-    // solver.solve_eq(test2);
+  // test2.Set_mesh_00008();
+  // solver.output_folder = "../output/domain_2/mesh0008/";
+  // solver.file_name = test2._name;
+  // solver.solve_eq(test2);
 
-    // test2.Set_mesh_001();    
-    // solver.output_folder = "../output/domain_2/mesh001_calfem/";
-    // solver.file_name = test2._name;
-    // solver.solve_eq(test2);
+  // test2.Set_mesh_00008_s();
+  // solver.output_folder = "../output/domain_2/mesh0008_s/";
+  // solver.file_name = test2._name;
+  // solver.solve_eq(test2);
 
-    // test2.Set_mesh_0001();    
-    // solver.output_folder = "../output/domain_2/mesh0001/";
-    // solver.file_name = test2._name;
-    // solver.solve_eq(test2);
+  // Test_domain_3_sin_sin<T> test3;
+  // solver.output_folder = "../output/domain_3/mesh001/";
+  // solver.file_name = test3._name;
+  // solver.solve_eq(test3);
 
-    // test2.Set_mesh_00008();    
-    // solver.output_folder = "../output/domain_2/mesh0008/";
-    // solver.file_name = test2._name;
-    // solver.solve_eq(test2);
+  /*
+      Dirichlet boundary tests
+  */
 
-    // test2.Set_mesh_00008_s();    
-    // solver.output_folder = "../output/domain_2/mesh0008_s/";
-    // solver.file_name = test2._name;
-    // solver.solve_eq(test2);
+  Test_domain_2_rectangle_dirichlet_only<T> test4;
 
+  // test4.Set_mesh_005();
+  // solver.output_folder = "../output/domain_2_extra/mesh005/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
+  // test4.Set_mesh_005_rev_calfem();
+  // solver.output_folder = "../output/domain_2_extra/mesh005_calfem/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
-    // Test_domain_3_sin_sin<T> test3;
-    // solver.output_folder = "../output/domain_3/mesh001/";
-    // solver.file_name = test3._name;
-    // solver.solve_eq(test3);
+  // test4.Set_mesh_005_calfem();
+  // solver.output_folder = "../output/domain_2_extra/mesh005_calfem/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
+  // test4.Set_mesh_001();
+  // solver.output_folder = "../output/domain_2_extra/mesh001/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
+  // test4.Set_mesh_001_calfem();
+  // solver.output_folder = "../output/domain_2_extra/mesh001_calfem/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
-    /*
-        Dirichlet boundary tests
-    */
+  // test4.Set_mesh_0005_calfem();
+  // solver.output_folder = "../output/domain_2_extra/mesh0005_calfem/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
+  // test4.Set_mesh_0001();
+  // solver.output_folder = "../output/domain_2_extra/mesh0001/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
+  test4.Set_mesh_0001_calfem();
+  solver.output_folder = "../output/domain_2_extra/mesh0001_calfem/";
+  solver.file_name = test4._name;
+  solver.solve_eq_dirichlet_only(test4);
 
-    Test_domain_2_rectangle_dirichlet_only<T> test4;
+  // test4.Set_mesh_00005_calfem();
+  // solver.output_folder = "../output/domain_2_extra/mesh00005_calfem/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
-    // test4.Set_mesh_005();
-    // solver.output_folder = "../output/domain_2_extra/mesh005/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // test4.Set_mesh_00008();
+  // solver.output_folder = "../output/domain_2_extra/mesh00008/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
-    // test4.Set_mesh_005_rev_calfem();
-    // solver.output_folder = "../output/domain_2_extra/mesh005_calfem/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // test4.Set_mesh_00008_calfem();
+  // solver.output_folder = "../output/domain_2_extra/mesh00008_calfem/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
-    // test4.Set_mesh_005_calfem();
-    // solver.output_folder = "../output/domain_2_extra/mesh005_calfem/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // test4.Set_mesh_00008_s();
+  // solver.output_folder = "../output/domain_2_extra/mesh00008_s/";
+  // solver.file_name = test4._name;
+  // solver.solve_eq_dirichlet_only(test4);
 
-    // test4.Set_mesh_001();
-    // solver.output_folder = "../output/domain_2_extra/mesh001/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // Test_domain_1_1_sin<T> test1_1;
+  // test1_1.Set_mesh_0005_calfem();
+  // solver.output_folder = "../output/domain_1_1/mesh0005_calfem/";
+  // solver.file_name = test1_1._name;
+  // solver.solve_eq(test1_1);
 
-    // test4.Set_mesh_001_calfem();
-    // solver.output_folder = "../output/domain_2_extra/mesh001_calfem/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // test1_1.Set_mesh_0005_3_in_row_calfem();
+  // solver.output_folder = "../output/domain_1_1/mesh0005_3_in_row_calfem/";
+  // solver.file_name = test1_1._name;
+  // solver.solve_eq(test1_1);
 
-    // test4.Set_mesh_0005_calfem();
-    // solver.output_folder = "../output/domain_2_extra/mesh0005_calfem/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // test1_1.Set_mesh_0001_calfem();
+  // solver.output_folder = "../output/domain_1_1/mesh0001_calfem/";
+  // solver.file_name = test1_1._name;
+  // solver.solve_eq(test1_1);
 
-    // test4.Set_mesh_0001();
-    // solver.output_folder = "../output/domain_2_extra/mesh0001/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // test1_1.Set_mesh_0001_3_in_row_calfem();
+  // solver.output_folder = "../output/domain_1_1/mesh0001_3_in_row_calfem/";
+  // solver.file_name = test1_1._name;
+  // solver.solve_eq(test1_1);
 
-    test4.Set_mesh_0001_calfem();
-    solver.output_folder = "../output/domain_2_extra/mesh0001_calfem/";
-    solver.file_name = test4._name;
-    solver.solve_eq_dirichlet_only(test4);
+  // Test_domain_1_1_1_sin<T> test1_1_1;
+  // solver.output_folder = "../output/domain_1_1_1/mesh001/";
+  // solver.file_name = test1_1_1._name;
+  // solver.solve_eq(test1_1_1);
 
-    // test4.Set_mesh_00005_calfem();
-    // solver.output_folder = "../output/domain_2_extra/mesh00005_calfem/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // Test_domain_4_abs_x<T> test5;
 
-    // test4.Set_mesh_00008();
-    // solver.output_folder = "../output/domain_2_extra/mesh00008/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // test5.Set_mesh001_calfem();
+  // solver.output_folder = "../output/domain_4/mesh001_calfem/";
+  // solver.file_name = test5._name;
+  // solver.solve_eq(test5);
 
-    // test4.Set_mesh_00008_calfem();
-    // solver.output_folder = "../output/domain_2_extra/mesh00008_calfem/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  // solver.output_folder = "../output/domain_4/mesh001_3_in_row_calfem/";
+  // solver.file_name = test5._name;
+  // solver.solve_eq(test5);
 
-    // test4.Set_mesh_00008_s();
-    // solver.output_folder = "../output/domain_2_extra/mesh00008_s/";
-    // solver.file_name = test4._name;
-    // solver.solve_eq_dirichlet_only(test4);
+  t_end = clock();
 
-    
+  std::cout << ("Time taken: %.2fs\n",
+                (double)(t_end - t_start) / CLOCKS_PER_SEC)
+            << "\n";
 
-
-
-
-    // Test_domain_1_1_sin<T> test1_1;
-    // test1_1.Set_mesh_0005_calfem();
-    // solver.output_folder = "../output/domain_1_1/mesh0005_calfem/";
-    // solver.file_name = test1_1._name;
-    // solver.solve_eq(test1_1);
-
-    // test1_1.Set_mesh_0005_3_in_row_calfem();
-    // solver.output_folder = "../output/domain_1_1/mesh0005_3_in_row_calfem/";
-    // solver.file_name = test1_1._name;
-    // solver.solve_eq(test1_1);
-
-    // test1_1.Set_mesh_0001_calfem();
-    // solver.output_folder = "../output/domain_1_1/mesh0001_calfem/";
-    // solver.file_name = test1_1._name;
-    // solver.solve_eq(test1_1);
-
-    // test1_1.Set_mesh_0001_3_in_row_calfem();
-    // solver.output_folder = "../output/domain_1_1/mesh0001_3_in_row_calfem/";
-    // solver.file_name = test1_1._name;
-    // solver.solve_eq(test1_1);
-
-
-    // Test_domain_1_1_1_sin<T> test1_1_1;
-    // solver.output_folder = "../output/domain_1_1_1/mesh001/";
-    // solver.file_name = test1_1_1._name;
-    // solver.solve_eq(test1_1_1);
-
-
-    // Test_domain_4_abs_x<T> test5;
-
-    // test5.Set_mesh001_calfem();
-    // solver.output_folder = "../output/domain_4/mesh001_calfem/";
-    // solver.file_name = test5._name;
-    // solver.solve_eq(test5);
-
-
-    // solver.output_folder = "../output/domain_4/mesh001_3_in_row_calfem/";
-    // solver.file_name = test5._name;
-    // solver.solve_eq(test5);
-    
-
-    
-    t_end = clock();
-
-    std::cout << ("Time taken: %.2fs\n", (double)(t_end - t_start)/CLOCKS_PER_SEC) << "\n";
-
-    return 0;
+  return 0;
 }
